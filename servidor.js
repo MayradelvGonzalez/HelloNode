e887ff98c56a20310812ed924f88ac34cbd43528
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-04-08 23:27:45 -0300

    createServer

---
+++ servidor.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((require, response) => {
+    response.end("Hello World from the backend!");
+})
+
+server.listen(8080,()=>
+console.log('Listening on PORT 8080')
+)
